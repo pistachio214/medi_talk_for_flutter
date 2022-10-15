ba88d44d632b7c0b0dd28705608371b1e391a520
Roger Peng <aileshang0226@163.com>
2022-10-15 23:44:08 +0800

    update 注册界面

--- lib/views/login/index.dart
+++ lib/views/login/index.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:medi_talk_for_flutter/lang/const.dart';
 import 'package:medi_talk_for_flutter/utils/color_util.dart';
+import 'package:medi_talk_for_flutter/utils/logs_util.dart';
 
 class InputState {
-  final bool invalid = false;
+  final bool invalid;
 
-  final String value = "";
+  final String value;
+
+  InputState({this.invalid = false, this.value = ""});
 }
 
 class Login extends StatefulWidget {
@@ -17,6 +21,8 @@ class Login extends StatefulWidget {
 
 class _LoginState extends State<Login> {
   late InputState username = InputState();
+
+  late InputState password = InputState();
 
   @override
   Widget build(BuildContext context) {
@@ -55,12 +61,17 @@ class _LoginState extends State<Login> {
           ),
           Container(
             width: size.width,
-            margin: const EdgeInsets.only(top: 40, left: 25),
+            margin: const EdgeInsets.only(
+              left: 25,
+              right: 25,
+              top: 40,
+              bottom: 15,
+            ),
             child: Column(
               children: [
                 Container(
                   alignment: Alignment.bottomLeft,
-                  margin: const EdgeInsets.only(top: 25),
+                  margin: const EdgeInsets.symmetric(vertical: 15),
                   child: Text(
                     "Log in",
                     style: TextStyle(
@@ -71,19 +82,139 @@ class _LoginState extends State<Login> {
                   ),
                 ),
                 Container(
-                  color: Colors.red,
+                  margin: const EdgeInsets.symmetric(vertical: 15),
                   child: TextField(
+                    maxLength: 18,
+                    maxLines: 1,
                     decoration: InputDecoration(
-                      hintText: '用户名',
-                      contentPadding:
-                          const EdgeInsets.symmetric(horizontal: 10),
+                      hintText: 'E-mail Address',
+                      hintStyle: const TextStyle(
+                        color: Color.fromRGBO(25, 59, 104, 0.5),
+                      ),
+                      border: const UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Color.fromRGBO(25, 59, 104, 0.2),
+                          width: 1.5,
+                        ),
+                      ),
+                      counterText: "",
                       errorText: username.invalid ? '请输入用户名' : null,
                     ),
                   ),
-                )
+                ),
+                Container(
+                  margin: const EdgeInsets.symmetric(vertical: 15),
+                  child: TextField(
+                    maxLength: 18,
+                    maxLines: 1,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: 'Password',
+                      hintStyle: const TextStyle(
+                        color: Color.fromRGBO(25, 59, 104, 0.5),
+                      ),
+                      border: const UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Color.fromRGBO(25, 59, 104, 0.2),
+                          width: 1.5,
+                        ),
+                      ),
+                      counterText: "",
+                      errorText: password.invalid ? '请输入密码' : null,
+                    ),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                    style: ButtonStyle(
+                      overlayColor:
+                      MaterialStateProperty.resolveWith((states) {
+                        return Colors.transparent;
+                      }),
+                    ),
+                    onPressed: () {
+                      LogsUtil.info("忘记密码");
+                    },
+                    child: const Text(
+                      'Forget?',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w700,
+                        fontSize: 14,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.symmetric(vertical: 15),
+                  width: size.width - 50,
+                  height: 55,
+                  decoration: BoxDecoration(
+                    color: ColorUtil.hexToColor(Const.defaultSystemThemeColor),
+                    borderRadius: const BorderRadius.all(Radius.circular(10)),
+                  ),
+                  child: TextButton(
+                    style: ButtonStyle(
+                      shape: MaterialStateProperty.all(
+                        const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                        ),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: Text(
+                      "Log In",
+                      style: TextStyle(
+                        color: ColorUtil.hexToColor("#FFFFFF"),
+                        fontWeight: FontWeight.w700,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ),
               ],
             ),
-          )
+          ),
+          Expanded(
+            child: Container(
+              alignment: Alignment.bottomCenter,
+              margin: const EdgeInsets.symmetric(horizontal: 25),
+              child: Container(
+                margin: const EdgeInsets.only(bottom: 50),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    const Text(
+                      "Haven't any account?",
+                      style: TextStyle(
+                        color: Color.fromRGBO(25, 59, 104, 0.7),
+                        fontWeight: FontWeight.w400,
+                        fontSize: 16,
+                      ),
+                    ),
+                    TextButton(
+                      style: ButtonStyle(
+                        overlayColor:
+                            MaterialStateProperty.resolveWith((states) {
+                          return Colors.transparent;
+                        }),
+                      ),
+                      onPressed: () {
+                        LogsUtil.info("前往注册");
+                      },
+                      child: const Text(
+                        "Sign Up",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 16,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
